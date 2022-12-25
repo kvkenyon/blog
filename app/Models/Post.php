709b7621d9aeba55f1cb5e8e103667249584ad94
Kevin Kenyon <kvkenyon@gmail.com>
2022-12-25 10:47:53 -0600

    Initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+class Post
+{
+    public static function find($slug)
+    {
+        return "hello world";
+    }
+}
---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="/app.css" rel="stylesheet"/>
+    </head>
+    <title>My Blog</title>
+    <body>
+        <?= $post; ?>
+        <a href="/">Go back</a>
+    </body>
+</html>
