e511ed97d902ea793edec5b7eac86bddc3d71b0a
Kevin Kenyon <kvkenyon@gmail.com>
2022-12-26 16:50:21 -0600

    Add slug to post for routing

--- database/migrations/2022_12_26_164511_create_posts_table.php
+++ database/migrations/2022_12_26_164511_create_posts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('excerpt');
             $table->text('body');
+            $table->string('slug')->unique();
             $table->timestamps();
             $table->timestamp('published_at')->nullable();
         });
